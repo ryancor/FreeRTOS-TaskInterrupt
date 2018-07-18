b92f53b5214f3c73511bfc2a1d2b8a0245622b0b
ryancor <ryan_cornateanu@yahoo.com>
2018-07-18 14:20:45 -0700

    updated compiled arm binary

--- Objects/main.d
+++ Objects/main.d
@@ -18,6 +18,7 @@
 .\objects\main.o: ..\FreeRTOS-Source\include\task.h
 .\objects\main.o: ..\FreeRTOS-Source\include\list.h
 .\objects\main.o: ..\FreeRTOS-Source\include\queue.h
+.\objects\main.o: ..\FreeRTOS-Source\include\semphr.h
 .\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.11.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h
 .\objects\main.o: .\RTE\Device\STM32F407VGTx\stm32f4xx_hal_conf.h
 .\objects\main.o: .\RTE\_FreeRTOS-Queues\RTE_Components.h
